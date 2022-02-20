9ebed860095e6ec6f0e19e2ebd8c8802b1d2973d
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-20 17:38:07 +0900

    add get/set/constracter

--- src/main/java/com/example/demo/entity/Post.java
+++ src/main/java/com/example/demo/entity/Post.java
@@ -5,10 +5,63 @@ import java.time.LocalDateTime;
 public class Post {
 	private String music_name;
 	private String artist_name;
+	private PostComment postComment;
 	private int genre;
 	private String caption;
 	private LocalDateTime created;
 	
+	
+//	コンストラクタ(引数なし)
+	public Post() {
+		
+	}
+	
 
+//	getter/setter
+	
+	public String getMusic_name() {
+		return music_name;
+	}
+	public void setMusic_name(String music_name) {
+		this.music_name = music_name;
+	}
+	public String getArtist_name() {
+		return artist_name;
+	}
+	public void setArtist_name(String artist_name) {
+		this.artist_name = artist_name;
+	}
+	
+	public PostComment getPostComment() {
+		return postComment;
+	}
+
+
+	public void setPostComment(PostComment postComment) {
+		this.postComment = postComment;
+	}
+
+
+	public int getGenre() {
+		return genre;
+	}
+	public void setGenre(int genre) {
+		this.genre = genre;
+	}
+	public String getCaption() {
+		return caption;
+	}
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+	public LocalDateTime getCreated() {
+		return created;
+	}
+	public void setCreated(LocalDateTime created) {
+		this.created = created;
+	}
+	
+
+	
 
 }
