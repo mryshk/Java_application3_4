6cfb87e1919681609635ad1fd23eba98667db9da
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-20 17:38:11 +0900

    add get/set/constracter

--- src/main/java/com/example/demo/entity/PostComment.java
+++ src/main/java/com/example/demo/entity/PostComment.java
@@ -5,8 +5,45 @@ import java.time.LocalDateTime;
 public class PostComment {
 	
 	private int userId;
-	private int PostId;
+	private int postId;
 	private String comment;
 	private LocalDateTime created;
+	
+	
+	
+	
+	
+//	コンストラクタ
+	public PostComment() {
+		
+	}
+
+//	getter/setter
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getPostId() {
+		return post_id;
+	}
+	public void setPostId(int postId) {
+		postId = postId;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public LocalDateTime getCreated() {
+		return created;
+	}
+	public void setCreated(LocalDateTime created) {
+		this.created = created;
+	}
+	
 
 }
