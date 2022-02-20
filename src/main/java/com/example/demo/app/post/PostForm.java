78e21383f508fab2b4cc3618ac2878ec81cfb917
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-20 17:46:26 +0900

    modify

--- src/main/java/com/example/demo/app/post/PostForm.java
+++ src/main/java/com/example/demo/app/post/PostForm.java
@@ -1,5 +1,23 @@
 package com.example.demo.app.post;
 
+import java.time.LocalDateTime;
+
+import com.example.demo.entity.PostComment;
+
+
 public class PostForm {
+	
+	@NOT NULL
+	private String music_name;
+	
+	private String artist_name;
+	
+	private PostComment postComment;
+	
+	private int genre;
+	
+	private String caption;
+	
+	private LocalDateTime created;
 
 }
