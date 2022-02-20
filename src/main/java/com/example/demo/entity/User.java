20c6798f706040d0d71bb19ba6ad928bbe9a8251
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-20 16:01:20 +0900

    add entity

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,11 @@
+package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+
+public class User {
+	private String name;
+	private String email;
+	private String password;
+	private LocalDateTime created;
+
+}
