92a9843a3fbeba85cf54c817e25d4284a142576c
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-20 16:01:27 +0900

    add entity

--- src/main/java/com/example/demo/entity/Post.java
+++ src/main/java/com/example/demo/entity/Post.java
@@ -1,9 +1,14 @@
 package com.example.demo.entity;
 
+import java.time.LocalDateTime;
+
 public class Post {
 	private String music_name;
 	private String artist_name;
 	private int genre;
+	private String caption;
+	private LocalDateTime created;
+	
 
 
 }
