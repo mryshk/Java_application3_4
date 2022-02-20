293b73a553847372befb4f4b775b1f3c236c4853
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-20 16:01:24 +0900

    add entity

---
+++ src/main/java/com/example/demo/entity/PostComment.java
@@ -0,0 +1,12 @@
+package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+
+public class PostComment {
+	
+	private int userId;
+	private int PostId;
+	private String comment;
+	private LocalDateTime created;
+
+}
