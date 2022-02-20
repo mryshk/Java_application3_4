a49a3cdd8e86051d39b5bdf01c322320d906c7a2
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-20 17:38:15 +0900

    add get/set/constracter

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -7,5 +7,45 @@ public class User {
 	private String email;
 	private String password;
 	private LocalDateTime created;
+	
+	
+	
+	
+	
+//	コンストラクタ
+	public User() {
+		
+	}
+	
+//	getter/setter
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public LocalDateTime getCreated() {
+		return created;
+	}
+	public void setCreated(LocalDateTime created) {
+		this.created = created;
+	}
+	
+	
+	
+	
+	
 
 }
