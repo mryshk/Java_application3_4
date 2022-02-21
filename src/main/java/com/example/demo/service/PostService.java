37798e13822840d424a987e3abecbb16c3dbec83
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-21 16:38:09 +0900

    finish service

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -1,5 +1,22 @@
 package com.example.demo.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.entity.Post;
+
 public interface PostService {
+	
+	List<Post> findAll();
+	
+	Optional <Post> getPost(int id);
+
+	void insert(Post post);
+
+	void update(Post post);
+
+	void deleteById(int id);
+	
+
 
 }
--- src/main/java/com/example/demo/service/PostServiceImpl.java
+++ src/main/java/com/example/demo/service/PostServiceImpl.java
@@ -1,5 +1,58 @@
 package com.example.demo.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.PostDao;
+import com.example.demo.entity.Post;
+
+@Service
 public class PostServiceImpl implements PostService {
+	
+	private final PostDao dao;
+	
+	public PostServiceImpl(PostDao dao) {
+		this.dao = dao;
+	}
+
+	@Override
+	public List<Post> findAll() {
+		return dao.findAll();
+	}
+
+	@Override
+	public Optional<Post> getPost(int id) {
+		try {
+			return dao.findById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new PostNotFoundException("指定された投稿はありません。");
+		}
+		
+	}
+
+	@Override
+	public void insert(Post post) {
+		dao.insert(post);
+		
+	}
+
+	@Override
+	public void update(Post post) {
+		if(dao.update(post) == 0) {
+			throw new PostNotFoundException("指定された投稿はありません。");
+		}
+		
+	}
+
+	@Override
+	public void deleteById(int id) {
+		if (dao.deleteById(id) == 0) {
+			throw new PostNotFoundException("指定された投稿はありません");
+		}
+		
+	}
 
 }
