a79913b8be7d60c5a39c7b8c0459c0cd5dc8e59b
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-23 18:56:16 +0900

    modify

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,12 @@
 
 
-INSERT INTO post(user_id, music_name, artist_name, genre, caption,created)
-VALUES(1, 'ok', 'King gnu', 1,'ロックバンドです。','2021-12-03 12:00:00');
+INSERT INTO post(id,user_id, music_name, artist_name, genre, caption,created)
+VALUES(1,1, '一途', 'King gnu',1, 'ロックバンドです。','2021-12-03 12:00:00');
+
+
+INSERT INTO user(id,name, email, password)
+VALUES(1,'TARO', 'taro@taro.com', 'tarotarotaro');
+
+
+INSERT INTO postComment(id,post_id, user_id, comment, created)
+VALUES(1, 1, 1, 'Good', '2022-02-22 12:00:00');
