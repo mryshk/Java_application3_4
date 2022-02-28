5ab2e1321cf0cd84f4112ad15811fd2ea8c77ccb
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-03-01 00:48:51 +0900

    modify

---
+++ src/main/java/com/example/demo/entity/GenreType.java
@@ -0,0 +1,41 @@
+package com.example.demo.entity;
+
+public class GenreType {
+	
+	private int id;
+	private String genre;
+	
+	
+	public GenreType() {}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getGenre() {
+		return genre;
+	}
+
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+
+}
